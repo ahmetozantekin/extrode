7e3fb0204d0bd6f3ad4ab6dce4b0d894ff77c86f
Okan Vurdu <okanvurdu@gmail.com>
2016-08-04 08:56:11 +0300

    ESLint was integrated

--- extrode.js
+++ extrode.js
@@ -2,26 +2,26 @@
 
 module.exports = function (object, keys) {
 
-    // Check the object whether is array or not
-    if (object.constructor === Array) {
-        return object.map(function (item) {
-            return extractKeys(item, keys);
-        });
-    }
-    else if(object.constructor === Object){
-        return extractKeys(object, keys);
-    }
-    else {
-        throw 'Type of first parameter must be "array" or "object"';
-    }
+	// Check the object whether is array or not
+	if (object.constructor === Array) {
+		return object.map(function (item) {
+			return extractKeys(item, keys);
+		});
+	}
+	else if(object.constructor === Object){
+		return extractKeys(object, keys);
+	}
+	else {
+		throw 'Type of first parameter must be "array" or "object"';
+	}
 
-    function extractKeys(obj, keys){
-        var newObj = Object.create(null);
-        for (var key in obj){
-            if(keys.indexOf(key) != -1){
-                newObj[key] = obj[key];
-            }
-        }
-        return newObj;
-    }
+	function extractKeys(obj, keys){
+		var newObj = Object.create(null);
+		for (var key in obj){
+			if(keys.indexOf(key) != -1){
+				newObj[key] = obj[key];
+			}
+		}
+		return newObj;
+	}
 };
