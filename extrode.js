a94fc9b98325259d99e1ee1b3c4588aa22ea50c0
Okan Vurdu <okanvurdu@gmail.com>
2016-08-04 07:43:35 +0300

    Object type check was replaced with contructure check

--- extrode.js
+++ extrode.js
@@ -2,28 +2,26 @@
 
 module.exports = function (object, keys) {
 
-    var typeOfObject = Object.prototype.toString.call(object);
-
     // Check the object whether is array or not
-    if (typeOfObject === '[object Array]') {
+    if (object.constructor === Array) {
         return object.map(function (item) {
-            return buildSingleObject(item, keys);
+            return extractKeys(item, keys);
         });
     }
-    else if(typeOfObject === '[object Object]'){
-        return buildSingleObject(object, keys);
+    else if(object.constructor === Object){
+        return extractKeys(object, keys);
     }
     else {
         throw 'Type of first parameter must be "array" or "object"';
     }
 
-    function buildSingleObject(obj, keys){
-        var newObject = Object.create(null);
+    function extractKeys(obj, keys){
+        var newObj = Object.create(null);
         for (var key in obj){
             if(keys.indexOf(key) != -1){
-                newObject[key] = obj[key];
+                newObj[key] = obj[key];
             }
         }
-        return newObject;
+        return newObj;
     }
 };
