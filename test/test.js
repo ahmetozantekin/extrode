70ce537e4b8fba49bcb42a0e984a835ad1a0126b
Mustafa Uzun <mustafauzun0@gmail.com>
2018-10-02 00:58:08 +0300

    Code Changes
    #test\test.js
    - parameter test Removed
    
    #extrode.js
    - Make prototype
    - Add es6 functions
    - Type throw Removed
    - Change variable names
    - Change variable scope var to let
    - 'use strict' Removed

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ describe('#extrode', function(){
             { x: 1, y: 2 }, { x: 3, y: 4 }, { x: 5, y: 6}
         ];
 
-        var result = extrode(array, ['x']);
+        var result = array.extrode(['x']);
 
         expect(result[0]).to.have.all.keys('x');
 
@@ -18,18 +18,8 @@ describe('#extrode', function(){
 
     it('should extract keys from a single object', function(){
         var obj = { x: 1, y: 2 };
-        var result = extrode(obj, ['x']);
+        var result = obj.extrode(['x']);
 
         expect(result).to.have.all.keys('x');
     });
-
-    it('first given parameter must be an array or object', function(){
-        try {
-            extrode('dummy', ['x']);
-        }
-        catch(err) {
-            expect(err).to.eql('Type of first parameter must be "array" or "object"');
-        }
-    });
-
 });
